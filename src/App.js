12059f12552f7498b2b79a316340ed0f9e5e156f
Neel Prajapati <neel.prajapati@netanalytics.ai>
2023-06-20 10:49:37 +0530

    [RELEASE] v0.1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NetworkGraph from "./NetworkGraph";
+
+function App() {
+  return (
+    <div>
+      <NetworkGraph />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NetworkGraph.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ForceGraph3D from 'react-force-graph-3d';
+
+const NetworkGraph = () => {
+  // Generate nodes and links
+  const numNodes = 100000;
+  const numLinks = 1400000;
+  const nodes = [...Array(numNodes)].map((_, i) => ({ id: i }));
+  const links = [...Array(numLinks)].map((_, i) => ({
+    source: Math.floor(Math.random() * numNodes),
+    target: Math.floor(Math.random() * numNodes)
+  }));
+
+  return (
+    <ForceGraph3D
+      graphData={{ nodes, links }}
+      width={1900}
+      height={1000}
+      linkColor={() => 'rgba(255, 255, 255, 0.2)'}
+    />
+  );
+};
+
+export default React.memo(NetworkGraph);
